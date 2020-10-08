c3f366f47259794c08dd5935e600c3a5bad47b49
Victor <g.victor@hotmail.fr>
2020-10-08 17:52:22 +0200

    creation of quizz result pages

---
+++ page_results2.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>SoProno</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <?php include 'header.php'?>
+    
+    <section>
+       Work in progress results2
+    </section>
+    <footer>
+        <?php include 'footer.php'?>
+    </footer>
+</body>
+</html>
