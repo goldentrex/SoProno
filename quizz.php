912a0f45e83812d0de5d7faf9760619d4bbeed2a
Victor <g.victor@hotmail.fr>
2020-10-08 17:46:12 +0200

    changed quizz to host the links of quizz1 and quizz2

--- quizz.php
+++ quizz.php
@@ -7,7 +7,11 @@
     <?php include 'header.php'?>
     
     <section>
-       Work in progress quizz
+       Work in progress quizz <br/><br/>
+
+		<a class="link" href="page_quizz1.php"> Quizz 1 </a><br/>
+        <a class="link" href="page_quizz2.php"> Quizz 2 </a><br/>
+
     </section>
     <footer>
         <?php include 'footer.php'?>
