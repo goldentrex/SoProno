3d81ab78cc1690c2ea4636225a5cac016e850f15
Victor <g.victor@hotmail.fr>
2020-10-23 17:26:20 +0200

    added some stuff to homepage

--- homepage.php
+++ homepage.php
@@ -2,13 +2,30 @@
 <head>
     <title>SoProno</title>
     <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="home_page_style.css" />
 </head>
 <body>
     <?php include 'header.php'?>
     
     <section>
-       Work in progress homepage
+       <br/>
     </section>
+
+    <news>
+    	<div class="newsTitle"> News EURO 2020</div><br />
+         <div class="newsContent">
+            <img class ="imgnews" src="./img/news/ballon.jpg" alt="ballon">
+            Le nouveau ballon est plus beau que jamais !
+            <br />
+            <img class ="imgnews" src="./img/news/mbappe.jpg" alt="mbappe">
+            Mais quelle vitesse extraordinaire pour le jeune joueur français !
+            <br />
+            <img class ="imgnews" src="./img/news/terrain.jpg" alt="terrain">
+            Le terrain est tout vert cette année, comme d'habitude...
+            <br />
+         </div>
+      </div><br />
+    </news>
     <footer>
         <?php include 'footer.php'?>
     </footer>
