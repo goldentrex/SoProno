90612d8fb05fdc83972a92b77e5192a7e75ff856
Jean <jean.de_guyon@isen.yncrea.fr>
2020-11-03 17:54:37 +0100

    style change

--- accountpage.php
+++ accountpage.php
@@ -33,17 +33,17 @@
         </p><br />
 
         <p> 
-            <label for="birth_date">Votre date de naissance :</label>
+            <label for="birth_date">Votre date de naissance :</label><br />
             <input type="date" name="birth_date" id="user_birth"/>
         </p><br />
 
         <p> 
-            <label for="password">Mot de passe :</label>
+            <label for="password">Mot de passe :</label><br />
             <input type="password" name="password" id="user_password" placeholder="Mot de passe" />
         </p><br />
 
         <p> 
-            <label for="password">Confirmez votre mot de passe :</label>
+            <label for="password">Confirmez votre mot de passe :</label><br />
             <input type="password" name="password2" id="password_confiramtion" placeholder="Confirmer le mot de passe"/>
         </p><br />
 
