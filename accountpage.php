c57a2b13bf7ec58c63e612a8e90d932026ba9f60
Jean <jean.de_guyon@isen.yncrea.fr>
2020-11-04 15:56:10 +0100

    connexion et import vers BDD

--- accountpage.php
+++ accountpage.php
@@ -15,7 +15,7 @@
     </div>
 
     <form method="post">
-        <p>
+        <!-- <p>
             <label for="name">Votre nom :</label><br />
             <input type="text" name="mail_adress" id="name" placeholder="Nom"/>
         </p><br />
@@ -24,35 +24,54 @@
         <p>
             <label for="prenom">Votre prénom :</label><br />
             <input type="text" name="prenom" id="user_name" placeholder="Prenom"/>
-        </p><br />
+        </p><br /> -->
 
         
         <p>
             <label for="mail">Votre adresse mail :</label><br />
-            <input type="mail" name="mail_adress" id="user_mail" placeholder="Mail"/>
+            <input type="mail" name="user_mail" id="user_mail" placeholder="Mail"/>
         </p><br />
 
-        <p> 
+        <!-- <p> 
             <label for="birth_date">Votre date de naissance :</label><br />
             <input type="date" name="birth_date" id="user_birth"/>
-        </p><br />
+        </p><br /> -->
 
         <p> 
             <label for="password">Mot de passe :</label><br />
-            <input type="password" name="password" id="user_password" placeholder="Mot de passe" />
+            <input type="password" name="password" id="password" placeholder="Mot de passe" required />
         </p><br />
 
         <p> 
             <label for="password">Confirmez votre mot de passe :</label><br />
-            <input type="password" name="password2" id="password_confiramtion" placeholder="Confirmer le mot de passe"/>
+            <input type="password" name="password2" id="password2" placeholder="Confirmer le mot de passe"/>
         </p><br />
 
-        <p><input type="submit" value="Inscription"></p>
-
-
-
-    
+        <p><input type="submit" name="inscription" value="inscription" id="inscription"></p>
     </form>
+
+
+    <?php 
+        if(isset($_POST['inscription'])){
+            extract($_POST);
+            if(!empty($user_mail)&&!empty($password)&&!empty($password2)){
+                if($password == $password2){
+                    include 'database.php';
+                    global $db;
+
+                    $query = $db->prepare("INSERT INTO users(user_mail,password) VALUES(:user_mail,:password)");
+                    $query->execute([
+                        'user_mail'=>$user_mail,
+                        'password' => $password
+                    ]);
+
+
+                }
+
+            }
+        }
+    ?>
+    
     <footer>
         <?php include 'footer.php'?>
     </footer>
--- database.php
+++ database.php
@@ -1,16 +1,16 @@
 <?php
 
     define('HOST','localhost');
-    define('DB_Name', 'Soprono');
+    define('DB_Name', 'soprono');
     define('USER', 'root');
     define('Password', '');
 
 
 
     try{
-        $db = new PDO("mysql:host=" . HOST . "dbname=" . DB_Name, USER, Password)
+        $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_Name, USER, Password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "connexion successful !"
+        echo "connexion successful !";
     } catch(PDOException $e){
         echo $e;
     }
