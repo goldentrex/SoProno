d747285d6afbac3cab5b3c00b2983849be6045fd
Charles <charles_sailliot62@outlook.fr>
2020-11-07 19:14:16 +0100

    Auto stash before merge of "php_v2" and "origin/php_v2"

--- controller/database.php
+++ controller/database.php
@@ -1,11 +1,11 @@
 <?php
 
 function dbConnection(){
-    
+
     $db_host='localhost';
     $db_user='root';
     $db_name ='soprono';
-    $db_password = '';
+    $db_password = 'root';
 
     try{
         $db = new PDO("mysql:host=" . $db_host . ";dbname=" .$db_name, $db_user, $db_password);
@@ -23,6 +23,7 @@ function dbConnection(){
 function getUserinformation($user_mail){
 
     $db = dbConnection();
+<<<<<<< Updated upstream:controller/database.php
  
     $query = $db->prepare('SELECT * FROM users WHERE user_mail=:user_mail');
     $query->execute(array(
@@ -38,3 +39,42 @@ function getUserinformation($user_mail){
 
 
 
+=======
+
+    if(isset($_POST['login'])){
+        if(!empty($user_mail) && !empty($password)){
+            $query = $db->prepare("SELECT * FROM users WHERE user_mail=:user_mail");
+            $query->execute(array(
+                'user_mail'=> $user_mail
+            ));
+
+            $data = $query->fetch(PDO::FETCH_ASSOC);
+        }
+    }
+    $query->closeCursor();
+    return $data;
+}
+
+
+// function getUserinformation(){
+//     require ('login.php');
+
+//     dbConnection();
+
+//     $query = $db->prepare('SELECT user_mail, password FROM users WHERE user_mail=:user_mail');
+//     $query->execute(array(
+//         'user_mail'=>$user_mail
+//     ));
+//     $result = $query->fetch();
+
+//     if($_POST['password'] == $result['password']){
+//         echo 'mot de passe incorrect';
+//     }
+//     else{
+//         session_start();
+//         $_SESSION['user_mail'] = $user_mail;
+//         echo 'vous êtes maintenant connecté !';
+//         header('Location : quizz.php');
+//     }
+// }
+>>>>>>> Stashed changes:model/database.php
--- view/page_quizz1.php
+++ view/page_quizz1.php
@@ -1,82 +1,93 @@
 <!DOCTYPE html>
 <html>
-<?php //include('header.php')?>
+<?php include('header.php')?>
 
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="view/css/style.css">
-    <title> Quizz 1 </title>
+    <link rel="stylesheet" href="style.css">
+    <title>Quizz1</title>
 </head>
 
 <body>
 
     <p class = "introduction"> Bonjour, vous trouverez ici un quizz concernant l'euro précédent. Il n'y a pas de meilleur préparation pour le prochain ! </p><br />
-   
+
     <br />
     <hr>
 
-    <form method="post" action="./view/page_results1.php">
-       
+    <form method="post" action="page_results1.php">
+
       <div>
-         <div class="question">
-            <label for="prefered_player">Quel est votre joueur préféré ? </label><br />
+         <div class="question1">
+            <label for="prefered_player">Le nom du capitaine du Portugal ? </label><br />
          </div><br />
                <div class="blocrep">
-                  <input type="text" name="prefered_player" id="prefered_player"/>
+                  <input type="text" name="capitainePortugal" id="capitainePortugal"/>
                </div>
       </div><br />
-      
+
       <hr>
 
 
-      <div><div class="question">Quel club est le plus ancien d'Europe ?</div><br />
+      <div><div class="question2">Quel club est le plus ancien d'Europe ?</div><br />
          <div class="blocrep">
-            <input type="radio" name="Real Madrid" id="club" value="Real Madrid" ><label for="Real Madrid">Real Madrid</label><br />
-            <input type="radio" name="PSG" id="club" value="PSG" ><label for="PSG">PSG</label><br />
-            <input type="radio" name="Manchester United" id="club" value="Manchester United" ><label for="Manchester United">Manchester United</label><br />
-            <input type="radio" name="Juventus Turin" id="club" value="Juventus Turin" ><label for="Juventus Turin">Juventus Turin</label>
+            <input type="radio" name="answerQ2[]" id="club" value="Real Madrid" ><label for="Real Madrid">Real Madrid</label><br />
+            <input type="radio" name="answerQ2[]" id="club" value="PSG" ><label for="PSG">PSG</label><br />
+            <input type="radio" name="answerQ2[]" id="club" value="Manchester United" ><label for="Manchester United">Manchester United</label><br />
+            <input type="radio" name="answerQ2[]" id="club" value="Juventus Turin" ><label for="Juventus Turin">Juventus Turin</label>
          </div>
       </div>
       <br />
 
       <hr>
 
-      <div><label for="pays"><div class="question">Quel est le pays d'Europe qui a gagné le dernier Euro ?</div></label><br />
+      <div><label for="pays"><div class="question3">Quel est le pays d'Europe qui a gagné le dernier Euro ?</div></label><br />
          <div class="blocrep">
-            <select name="pays" id="pays">
-               <option value="france">France</option>
+            <select name="answerQ3" id="pays">
+               <option value="France">France</option>
                <option value="portugal">Portugal</option>
                <option value="espagne">Espagne</option>
             </select>
          </div>
       <br />
-      
+
       <hr>
 
 
-      <div><div class="question"><label>Combien de fois la france a gagné l'Euro ?</div></label><br />
+      <div><div class="question4"><label>Combien de fois la France a t'elle remporté l'Euro</div></label><br />
          <div class ="blocrep">
-            <input type="number" id_number=""/>
+            <input type="number" name ="answerQ4"/>s
          </div>
       </div><br />
 
       <hr>
 
-      <div><div class ="question"> Quel est le joueur qui a marqué ... de buts pendant le dernier euro:</div><br />
+      <div><div class ="question5"> Quel pays a remporté l'Euro en 2004 ?</div><br />
          <div class="blocrep">
-            <input type="checkbox" name="player1" id="player1" /> <label for="player1">Joueur 1</label><br />
-            <input type="checkbox" name="player2" id="player2" /> <label for="player2">Joueur 2</label><br />
-            <input type="checkbox" name="player3" id="player3" /> <label for="player3">Joueur 3</label><br />
+           <label>
+            <input type="radio" name="answerQ5[]" value="Allemagne" />Allemagne &nbsp;&nbsp;&nbsp;<br />
+          </label>
+          <label>
+            <input type="radio" name="answerQ5[]" value="Grèce" /> Grèce&nbsp;&nbsp;&nbsp;<br />
+          </label>
+          <label>
+            <input type="radio" name="answerQ5[]" value="Italie" /> Italie &nbsp;&nbsp;&nbsp;<br />
+          </label>
          </div>
       </div><br />
 
       <hr>
 
-      <div><div class="question"> Quel est le joueur qui a marqué ... de buts pendant le dernier euro:</div><br />
+      <div><div class="question6"> Quel est le joueur qui a marqué le but assasin en finale du dernier Euro:</div><br />
          <div class="blocrep">
-            <input type="checkbox" name="player1" id="player1" /> <label for="player1">Joueur 1</label><br />
-            <input type="checkbox" name="player2" id="player2" /> <label for="player2">Joueur 2</label><br />
-            <input type="checkbox" name="player3" id="player3" /> <label for="player3">Joueur 3</label><br />
+           <label>
+              <input type="radio" name="answerQ6[]" value ="Eder"/>Eder &nbsp;&nbsp;&nbsp;<br />
+          </label>
+          <label>
+            <input type="radio" name="answerQ6[]" value ="Hazard"/>Hazard &nbsp;&nbsp;&nbsp;<br />
+          </label>
+            <input type="radio" name="answerQ6[]"value="Ronaldo"/>Ronaldo &nbsp;&nbsp;&nbsp;<br />
+          </label>
          </div>
       </div><br />
 
@@ -90,9 +101,8 @@
 
 </body>
 
-
 <footer>
-   <?php //include('footer.php')?>
+   <?php include('footer.php')?>
 </footer>
 
 </html>
