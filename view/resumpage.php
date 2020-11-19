7d573be7dfaff0eb4bb71cee6232b4aa6c21e855
julien <julienmeunier_59@hotmail.fr>
2020-11-19 20:06:42 +0100

    debut resume page

---
+++ view/resumpage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+<title>Pass quizz resum page</title>
+    <link rel="stylesheet" href="view/css/register.css" />
+    <link rel="stylesheet" href="view/css/style.css" />
+</head>
+
+<body>
+    <div class="intro_account">
+        Sélectionner un des quizz : 
+    </div>
+
+    <p class="quizzquestion"> <a href="index.php?page=quizz1"> Quizz 1</a></p><br/>
+    <p class="quizzquestion"> <a href="index.php?page=quizz2"> Quizz 2</a></p><br/>
+      
+    
+</body>
+</html>
